761831d78ad2f563ced02bcf38ed4be93743af6d
Giri <110292603+giri223008@users.noreply.github.com>
2023-04-29 21:13:27 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"github.com/giri223008/myniceprogram/helpers"
+)
+
+/*
+func calculateValue(intChan chan int) {
+	randomNumber := helpers.randomNumber(10)
+
+}
+*/
+
+func main() {
+	//intChan :=make(chan int)
+	log.Println("hh")
+
+	var myVar helpers.someType
+	
+
+}
