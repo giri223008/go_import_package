f45f56c2463c90a65cfcba5d69db4cce005dc2a7
Giri <110292603+giri223008@users.noreply.github.com>
2023-04-29 22:06:12 +0530

    Add files via upload

---
+++ helpers/helpers.go
@@ -0,0 +1,16 @@
+package helpers
+
+//import "math/rand"
+
+type someType struct {
+	TypeName string
+	TypeNo int
+}
+
+/*
+func randomNumber(n int)int{
+	value := rand.Intn(n)
+
+	return value
+}
+*/
